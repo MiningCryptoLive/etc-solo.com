32695d634906b376f629ed3663f9ad9f5561dd69
MiningCryptoLive <celldr69@gmail.com>
2023-01-29 06:19:20 -0500

    Update payouts.js

--- www/app/controllers/account/payouts.js
+++ www/app/controllers/account/payouts.js
@@ -22,7 +22,7 @@ export default Ember.Controller.extend({
             t = e.getWithDefault("model.paymentCharts"),
             a = {
                 chart: {
-                    backgroundColor: "rgba(255, 255, 255, 0.1)",
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
                     type: "column",
                     marginRight: 10,
                     height: 200,
